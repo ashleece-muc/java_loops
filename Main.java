5f7530557b42fd36a1e193f896afd072d9ba7d4f
ashleece-muc <74447453+ashleece-muc@users.noreply.github.com>
2022-01-27 17:50:51 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+package com.ashleece;
+
+public class Main {
+
+    public static void main(String[] args) {
+String[] test = {"**********"};
+System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+
+        String[] test2 = {"*"};
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+        String[] test3 = {"*"};
+        System.out.println("*");
+        System.out.println(" ");
+        System.out.println("**");
+        System.out.println(" ");
+        System.out.println("***");
+        System.out.println(" ");
+        System.out.println("****");
+        System.out.println("*****");
+
+        String[] test4 = {"*"};
+        System.out.println("*");
+        System.out.println("");
+        System.out.println("***");
+        System.out.println("");
+        System.out.println("*****");
+        System.out.println("*********");
+
+    }
+
+}
